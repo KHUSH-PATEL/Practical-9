bd2feb6b58d1e43b663951640c3f7809154543bc
Khush <khush@simformsolutions.com>
2023-06-20 18:21:50 +0530

    All files for practical 9 included

---
+++ wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿function printText() {
+    let label = document.getElementById("text");
+    label.innerHTML = "Hello World";
+}
+
+function makeBold() {
+    let label = document.getElementById("text");
+    label.style.fontWeight = "bold";
+}
+
+function makeItalic() {
+    let label = document.getElementById("text");
+    label.style.fontStyle = "italic";
+}
+
+function makeUnderline() {
+    let label = document.getElementById("text");
+    label.style.textDecoration = "underline";
+}
+
+function reset() {
+    let label = document.getElementById("text");
+    label.innerHTML = "";
+    label.style.fontWeight = "normal";
+    label.style.fontStyle = "normal";
+    label.style.textDecoration = "none";
+}
